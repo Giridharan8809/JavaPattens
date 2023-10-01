370c8a35f8a214a863e2bc5adae8dbaa4ecef99e
giri <giri@DESKTOP-14LUHQ4>
2023-10-01 18:37:14 +0530

    Java_codes

---
+++ JavaCodes/src/Java_Pat/Patten01.java
@@ -0,0 +1,10 @@
+package Java_Pat;
+
+public class Patten01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaCodes/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author giri
+ *
+ */
+module JavaCodes {
+}
